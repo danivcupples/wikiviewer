4098db310a95bf0e8c7883d975cd6322cbf9c722
Dani <danielle.volker@gmail.com>
2017-06-24 17:50:22 -0400

    initial file creation, pseudocode notes

---
+++ wiki.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+//convert search terms to variable
+var input = document.getElementById('search-term')
+//api request to https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=
+//use search term variable to fill in titles
+
+//assign results to html.
+});
