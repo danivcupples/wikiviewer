92937b5fa05f6a6fd869e93751a02280bdff121b
Dani <danielle.volker@gmail.com>
2017-06-25 15:03:56 -0400

    form sort of working.

--- wiki.js
+++ wiki.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 //convert search terms to variable
-var input = document.getElementById('search-term')
+var input;
+$("form").submit(function(event){
+  input = $("#search-term").val();
+  alert(input);
+});
+console.log(input);
 //api request to https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=
+
 //use search term variable to fill in titles
 
 //assign results to html.
