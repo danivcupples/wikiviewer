5fdff62e151b6ec5d4ea43ed476186774e4981df
Dani <danielle.volker@gmail.com>
2017-06-26 15:59:49 -0400

    no real reason. still broken.

--- wiki.js
+++ wiki.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 //convert search terms to variable
-var input;
-$("form").submit(function(event){
-  input = $("#search-term").val();
-  alert(input);
+$("form").submit(function(){
+  var message = $('input[name=search-term]').val();
+  alert("You searched for " + message + "!");
+  return false;
+  console.log(message);
 });
-console.log(input);
 //api request to https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=
 
 //use search term variable to fill in titles
