c0be8e912b5eea332337bf11535e6e3559da8160
Dani <danielle.volker@gmail.com>
2017-06-27 14:40:01 -0400

    form works. api call works. functional use of data incomplete

--- wiki.js
+++ wiki.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 //convert search terms to variable
-$("form").submit(function(){
-  var message = $('input[name=search-term]').val();
-  alert("You searched for " + message + "!");
+var message;
+$("form").submit("click", function(){
+  message = $('input[name=search-term]').val();
+  //alert("You searched for " + message + "!");
+  var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&list=&origin=\*&titles=" + message + "&rvprop=content";
+
+  //api request to https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=
+  $.getJSON(url, function(response){
+    console.log(response.query.pages);
+  });
+
   return false;
-  console.log(message);
 });
-//api request to https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=
+
 
 //use search term variable to fill in titles
 
