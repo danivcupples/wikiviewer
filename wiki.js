64feee3f77edbeb1ce970ceda72b5c9fcb3d5fed
Dani <danielle.volker@gmail.com>
2017-06-29 16:10:58 -0400

    very little progress

--- wiki.js
+++ wiki.js
@@ -4,12 +4,18 @@ var message;
 $("form").submit("click", function(){
   message = $('input[name=search-term]').val();
   //alert("You searched for " + message + "!");
-  var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=\*&page=" + message + "&utf8=1";
+  var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=revisions&origin=\*&page=" + message;
 
   //api request to https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=
   $.getJSON(url, function(response){
-    console.log(response.parse.displaytitle);
-    console.log(response.parse.text);
+    console.log(response);
+
+    //pass response into reader field
+    const reader = document.querySelector("#content");
+    reader.innerHTML =
+      "<dl><dt>" + response.parse.title + "</dt>"
+      "<dd>" + response.parse.wikitext.* + "</dd></dl>"
+
   });
 
   return false;
