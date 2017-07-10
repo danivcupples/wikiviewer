bc17d9e093ff4d59ffcbcab9928199c9e153ef93
Dani <danielle.volker@gmail.com>
2017-07-10 15:58:37 -0400

    Switch to action=query prop=extracts. Trying to access variable data still.

--- wiki.js
+++ wiki.js
@@ -4,7 +4,7 @@ var message;
 $("form").submit("click", function(){
   message = $('input[name=search-term]').val();
   //alert("You searched for " + message + "!");
-  var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=revisions&origin=\*&page=" + message;
+  var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&exsentences=3&origin=\*&titles=" + message;
 
   //api request to https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=
   $.getJSON(url, function(response){
@@ -13,8 +13,8 @@ $("form").submit("click", function(){
     //pass response into reader field
     const reader = document.querySelector("#content");
     reader.innerHTML =
-      "<dl><dt>" + response.parse.title + "</dt>"
-      "<dd>" + response.parse.wikitext.* + "</dd></dl>"
+      "<dl><dt>" + response.query.pages[22954].title + "</dt>"
+      "<dd>" + response.query.pages[22954].extract + "</dd></dl>"
 
   });
 
